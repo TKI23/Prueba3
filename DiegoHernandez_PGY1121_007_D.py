1783a3837d0b4d5449e6553fc931142791ae38c1
TKI23 <138236265+TKI23@users.noreply.github.com>
2023-06-30 11:40:32 -0400

    Add files via upload

---
+++ DiegoHernandez_PGY1121_007_D.py
@@ -0,0 +1,37 @@
+#Evaluación N3
+
+import numpy as np
+
+Nombre=[];
+Rut=[];
+FechaNac=[];
+Categoria=[];
+Celular=[];
+IdenPar=[];
+opc=0
+
+
+def menu():
+    while True:
+        print(*25);
+        print('   - Registro Padel -   ');
+        print(*25);
+        print('1.- Grabar datos de jugador');
+        print('2.- Buscar un participante');
+        print('3.- Imprimir Parejas');
+        print('4.- Salir');
+        opc=(int, 'Seleccione una opción: '1, 4);
+        if opc==1
+            print('Primer Jugador')
+            Nombre.append(Nombre)
+            print('Rut')
+            Rut.append(Rut)
+            print('Fecha de Nacimiento')
+            FechaNac.append(FechaNac)
+            print('Categoría')
+            Categoria.append(Categoria)
+            print('Identificación de Parejas')
+            IdenPar.append(IdenPar)
+
+
+menu()
